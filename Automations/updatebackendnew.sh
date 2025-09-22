1b0b49c5153825feb6fcfb11bd451a01ed4f49a3
devi447 <devisingh447@gmail.com.com>
2025-09-23 00:34:50 +0530

    test

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -9,13 +9,13 @@ ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'R
 # Path to the .env file
 file_to_find="../backend/.env.docker"
 
-# Check the current FRONTEND_URL in the .env file
+# Check the current reactjs_URL in the .env file
 current_url=$(sed -n "4p" $file_to_find)
 
 # Update the .env file if the IP address has changed
-if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
+if [[ "$current_url" != "reactjs_URL=\"http://${ipv4_address}:5173\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+        sed -i -e "s|reactjs_URL.*|reactjs_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
