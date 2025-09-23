b98445eaeb0ed4e79d2b70b0f692d0e9c3d18159
devi447 <devisingh447@gmail.com.com>
2025-09-23 09:25:27 +0530

    vite react

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -7,15 +7,15 @@ INSTANCE_ID="i-08b2862319f08cdf5"
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
 # Path to the .env file
-file_to_find="../backend/.env.docker"
+file_to_find="../frontend/.env.docker"
 
-# Check the current FRONTEND_URL in the .env file
-current_url=$(sed -n "4p" $file_to_find)
+# Check the current VITE_API_PATH in the .env file
+current_url=$(cat $file_to_find)
 
 # Update the .env file if the IP address has changed
-if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
+if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
